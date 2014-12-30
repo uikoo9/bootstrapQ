2808efe15735b50ae9b09248e015a7bcb74ba8d1
uikoo9 <uikoo9@qq.com>
2014-12-30 14:33:42 +0800

    init

---
+++ bootstrapQ.js
@@ -0,0 +1,346 @@
+/**
+ * jquery的一些常用方法
+ * 1.qser
+ * 2.qdata
+ */
+$.fn.qser = function(){
+	var obj = {};
+	
+	var objs = $(this).serializeArray();
+	if(objs.length != 0){
+		for(var i=0; i<objs.length; i++){
+			obj[objs[i].name] = objs[i].value;
+		}
+	}
+
+	return obj;
+};
+$.fn.qdata = function(){
+	var res = {};
+	
+	var data = $(this).attr('data');
+	if(data){
+		var options = data.split(';');
+		for(var i=0; i<options.length; i++){
+			if(options[i]){
+				var opt = options[i].split(':');
+				res[opt[0]] = opt[1];
+			}
+		}
+	}
+	
+	return res;
+};
+
+/**
+ * 封装一些常用方法
+ * 1.ajax
+ * 2.on
+ */
+var qiao = {};
+
+qiao.ajaxoptions = {
+	url 	: '',
+	data 	: {},
+	type 	: 'post',
+	dataType: 'json',
+	async 	: false
+};
+qiao.ajaxopt = function(options){
+	var opt = $.extend({}, qiao.ajaxoptions);
+	if(typeof options == 'string'){
+		opt.url = options;
+	}else{
+		$.extend(opt, options);
+	}
+	
+	return opt;
+};
+qiao.ajax = function(options){
+	if(!options){
+		alert('need options');
+	}else{
+		var opt = qiao.ajaxopt(options);
+		opt.url = base + opt.url;
+		
+		var res;
+		$.ajax(opt).done(function(obj){res = obj;});
+		return res;
+	}
+};
+qiao.on = function(obj, event, func){
+	$(document).off(event, obj).on(event, obj, func);
+};
+
+/**
+ * 对bootstrap的封装
+ * 1.alert
+ * 2.confirm
+ * 3.dialog
+ * 4.msg
+ * 5.tooltip
+ * 6.popover
+ * 7.bstree
+ * 8.scrollspy
+ * 9.initimg
+ * 10.bstro
+ */
+bootstrapQ 	= {};
+bootstrapQ.modaloptions = {
+	url 	: '',
+	fade	: 'fade',
+	close	: true,
+	title	: 'title',
+	btn		: false,
+	okbtn	: '确定',
+	qubtn	: '取消',
+	msg		: 'msg',
+	big		: false,
+	show	: false,
+	remote	: false,
+	backdrop: 'static',
+	keyboard: true,
+	style	: ''
+};
+bootstrapQ.modalstr = function(opt){
+	var start = '<div class="modal '+opt.fade+'" id="bsmodal" tabindex="-1" role="dialog" aria-labelledby="bsmodaltitle" aria-hidden="true" style="position:fixed;top:20px;'+opt.style+'">';
+	if(opt.big){
+		start += '<div class="modal-dialog modal-lg"><div class="modal-content">';
+	}else{
+		start += '<div class="modal-dialog"><div class="modal-content">';
+	}
+	var end = '</div></div></div>';
+	
+	var head = '<div class="modal-header">';
+	if(opt.close){
+		head += '<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>';
+	}
+	head += '<h3 class="modal-title" id="bsmodaltitle">'+opt.title+'</h3></div>';
+	var body = '<div class="modal-body"><p><h4>'+opt.msg+'</h4></p></div>';
+	var foot = '<div class="modal-footer"><button type="button" class="btn btn-primary bsok">'+opt.okbtn+'</button>';
+	if(opt.btn){
+		foot += '<button type="button" class="btn btn-default bscancel">'+opt.qubtn+'</button>';
+	}
+	foot += '</div>';
+	
+	return start + head + body + foot + end;
+};
+bootstrapQ.alert = function(options, func){
+	// options
+	var opt = $.extend({}, bootstrapQ.modaloptions);
+	if(typeof options == 'string'){
+		opt.msg = options;
+	}else{
+		$.extend(opt, options);
+	}
+	opt.title = '提示';
+	
+	// add
+	$('body').append(bootstrapQ.modalstr(opt));
+	
+	// init
+	var $modal = $('#bsmodal'); 
+	$modal.modal(opt);
+	
+	// bind
+	qiao.on('button.bsok', 'click', function(){
+		if(func) func();
+		$modal.modal('hide');
+	});
+	qiao.on('#bsmodal', 'hidden.bs.modal', function(){
+		$modal.remove();
+	});
+	
+	// show
+	$modal.modal('show');
+};
+bootstrapQ.confirm = function(options, ok, cancel){
+	// options
+	var opt = $.extend({}, bootstrapQ.modaloptions);
+	if(typeof options == 'string'){
+		opt.msg = options;
+	}else{
+		$.extend(opt, options);
+	}
+	opt.btn = true;
+	opt.title = '确认操作';
+	
+	// append
+	$('body').append(bootstrapQ.modalstr(opt));
+	
+	// init
+	var $modal = $('#bsmodal'); 
+	$modal.modal(opt);
+	
+	// bind
+	qiao.on('button.bsok', 'click', function(){
+		if(ok) ok();
+		$modal.modal('hide');
+	});
+	qiao.on('button.bscancel', 'click', function(){
+		if(cancel) cancel();
+		$modal.modal('hide');
+	});
+	qiao.on('#bsmodal', 'hidden.bs.modal', function(){
+		$modal.remove();
+	});
+	
+	// show
+	$modal.modal('show');
+};
+bootstrapQ.dialog = function(options, func){
+	// options
+	var opt = $.extend({}, bootstrapQ.modaloptions, options);
+	opt.big = true;
+	// append
+	$('body').append(bootstrapQ.modalstr(opt));
+	
+	// ajax page
+	var html = qiao.ajax({url:options.url, dataType:'html'});
+	$('#bsmodal div.modal-body').empty().append(html);
+	
+	// init
+	var $modal = $('#bsmodal'); 
+	$modal.modal(opt);
+	
+	// bind
+	qiao.on('button.bsok', 'click', function(){
+		var flag = true;
+		if(func){
+			flag = func();
+		}
+		
+		if(flag){
+			$modal.modal('hide');
+		}
+	});
+	qiao.on('#bsmodal', 'hidden.bs.modal', function(){
+		$modal.remove();
+	});
+	
+	// show
+	$modal.modal('show');
+};
+bootstrapQ.msgoptions = {
+	msg  : 'msg',
+	type : 'info',
+	time : 2000
+};
+bootstrapQ.msgstr = function(msg, type){
+	return '<div class="alert alert-'+type+' alert-dismissible" role="alert" style="display:none;position:fixed;top:0;left:0;width:100%;z-index:2001;margin:0;text-align:center;" id="bsalert"><button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>'+msg+'</div>';
+};
+bootstrapQ.msg = function(options){
+	var opt = $.extend({},bootstrapQ.msgoptions);
+	
+	if(typeof options == 'string'){
+		opt.msg = options;
+	}else{
+		$.extend(opt, options);
+	}
+	
+	$('body').prepend(bootstrapQ.msgstr(opt.msg,opt.type));
+	$('#bsalert').slideDown();
+	setTimeout(function(){
+		$('#bsalert').slideUp(function(){
+			$('#bsalert').remove();
+		});
+	},opt.time);
+};
+bootstrapQ.popoptions = {
+	animation 	: true,
+	container 	: 'body',
+	content		: 'content',
+	html		: true,
+	placement	: 'bottom',
+	title		: '',
+	trigger		: 'hover'//click | hover | focus | manual.
+};
+$.fn.bstip = function(options){
+	var opt = $.extend({}, bootstrapQ.popoptions);
+	if(typeof options == 'string'){
+		opt.title = options;
+	}else{
+		$.extend(opt, options);
+	}
+	
+	$(this).data(opt).tooltip();
+};
+$.fn.bspop = function(options){
+	var opt = $.extend({}, bootstrapQ.popoptions);
+	if(typeof options == 'string'){
+		opt.content = options;
+	}else{
+		$.extend(opt, options);
+	}
+	
+	$(this).popover(opt);
+};
+bootstrapQ.bstrooptions = {
+	width 	: '500px',
+	html 	: 'true',
+	nbtext	: '下一步',
+	place 	: 'bottom',
+	title 	: '网站使用引导',
+	content : 'content'
+};
+bootstrapQ.bstroinit = function(selector, options, step){
+	if(selector){
+		var $element = $(selector);
+		if($element.length > 0){
+			var opt = $.extend({}, bootstrapQ.bstrooptions, options);
+			if(typeof options == 'string'){
+				opt.content = options;
+			}else{
+				$.extend(opt, options);
+			}
+			
+			$element.each(function(){
+				$(this).attr({
+					'data-bootstro-width'			: opt.width, 
+					'data-bootstro-title' 			: opt.title, 
+					'data-bootstro-html'			: opt.html,
+					'data-bootstro-content'			: opt.content, 
+					'data-bootstro-placement'		: opt.place,
+					'data-bootstro-nextButtonText'	: opt.nbtext,
+					'data-bootstro-step'			: step
+				}).addClass('bootstro');
+			});
+		}
+	}
+};
+bootstrapQ.bstroopts = {
+	prevButtonText : '上一步',
+	finishButton : '<button class="btn btn-lg btn-success bootstro-finish-btn"><i class="icon-ok"></i>完成</button>',
+	stopOnBackdropClick : false,
+	stopOnEsc : false
+};
+bootstrapQ.bstro = function(bss, options){
+	if(bss && bss.length > 0){
+		for(var i=0; i<bss.length; i++){
+			bootstrapQ.bstroinit(bss[i][0], bss[i][1], i);
+		}
+		
+		var opt = $.extend({}, bootstrapQ.bstroopts);
+		if(options){
+			if(options.hasOwnProperty('pbtn')){
+				opt.prevButtonText = options.pbtn;
+			}
+			if(options.hasOwnProperty('obtn')){
+				if(options.obtn == ''){
+					opt.finishButton = '';
+				}else{
+					opt.finishButton = '<button class="btn btn-mini btn-success bootstro-finish-btn"><i class="icon-ok"></i>'+options.obtn+'</button>';
+				}
+			}
+			if(options.hasOwnProperty('stop')){
+				opt.stopOnBackdropClick = options.stop;
+				opt.stopOnEsc = options.stop;
+			}
+			if(options.hasOwnProperty('exit')){
+				opt.onExit = options.exit;
+			}
+		}
+		
+		bootstro.start('.bootstro', opt);
+	}
+};
